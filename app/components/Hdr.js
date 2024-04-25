36504921047b50c85500c13501f38f9e86d31233
bnz18523 <123269950+bnz18523@users.noreply.github.com>
2024-04-25 12:52:32 -0400

    Add files via upload

--- app/components/Hdr.js
+++ app/components/Hdr.js
@@ -13,7 +13,7 @@ const Hdr = () => {
       </nav>
       <div className="hdr">
         <h1>Insight Journal</h1>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTpVSRKza8xRQ1sRf6NWU_JqGgFmph_2eCtA&s" alt="logo"/>
+        <img src="https://cdn-sharing.adobecc.com/composite/component/id/urn:aaid:sc:VA6C2:39695009-7121-46b3-8734-bd19afec2d5e?component_id=688be569-be5c-4412-9532-5a80b29e822b&revision=3e9df177-ad68-45e5-8a08-20ec5d1403ed&api_key=projectx_webapp&access_token=1714105502_urn%3Aaaid%3Asc%3AVA6C2%3A39695009-7121-46b3-8734-bd19afec2d5e%3Bpublic_1d23fa11f89773aa550981739dd8911fb7a151cf" alt="logo"/>
       </div>
     </div>
   );
